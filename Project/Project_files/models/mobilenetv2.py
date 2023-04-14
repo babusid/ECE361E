9383226bb8a75b4376fa117ae14b4b41a25aa9af
Sidharth N. Babu <sidharth.n.babu@utexas.edu>
2023-04-14 13:16:01 -0500

    i'm stupid and forgot to make the forward function

--- Project/Project_files/models/mobilenetv2.py
+++ Project/Project_files/models/mobilenetv2.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torch.nn import ReLU6
+from torch.nn import ReLU6, ReLU
 
 
 
@@ -77,10 +77,14 @@ class MobileNetv2(nn.Module):
         super(MobileNetv2, self).__init__()
         self.conv1 = nn.Conv2d(3,32, kernel_size=3, stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(32)
+        self.relu1 = ReLU6()
+
         self.layers = self._make_layers(in_planes=32)
+        
         self.conv2 = nn.Conv2d(320, 1280, kernel_size=1, stride=1, padding=0, bias=False)
         self.avgpool = nn.AvgPool2d((7,7))
         self.conv3 = nn.Conv2d(1280, 1280, kernel_size=1, stride=1, padding=0, bias=False)
+        
         self.linear = nn.Linear(1280, num_classes)
     
     def _make_layers(self, in_planes):
@@ -92,3 +96,15 @@ class MobileNetv2(nn.Module):
             layers.append(Bottleneck(in_planes, out_planes, stride, expansion))
             in_planes = out_planes
         return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu1(out)
+        out = self.layers(out)
+        out = self.conv2(out)
+        out = self.avgpool(out)
+        out = self.conv3(out)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
